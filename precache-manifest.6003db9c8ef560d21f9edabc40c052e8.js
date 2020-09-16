9226adfc70fc7d8ee4a12209fc461186885502a5
ChaitraL99 <chaitra.lakshminarayan99@gmail.com>
2020-09-16 18:59:18 +0530

    Updates

---
+++ precache-manifest.6003db9c8ef560d21f9edabc40c052e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b3d4c33e7129f91eda5a883c298f7f",
+    "url": "/displayUserInfo/index.html"
+  },
+  {
+    "revision": "719ffc4ab950182bd5c2",
+    "url": "/displayUserInfo/static/css/main.32565694.chunk.css"
+  },
+  {
+    "revision": "7efbe3664406206e1d62",
+    "url": "/displayUserInfo/static/js/2.20d4d77e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/displayUserInfo/static/js/2.20d4d77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719ffc4ab950182bd5c2",
+    "url": "/displayUserInfo/static/js/main.e9fad1ab.chunk.js"
+  },
+  {
+    "revision": "3cd702ab31b970eb1d6e",
+    "url": "/displayUserInfo/static/js/runtime-main.212e9777.js"
+  }
+]);
